8c0fb855b92bcea03bcd04fd351427247685f68d
Ruben Taelman <rubensworks@gmail.com>
2014-11-19 16:42:25 +0100

    Implement graph approach.

--- bin/startLiveMusicServer.sh
+++ bin/startLiveMusicServer.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+SERVER="/Users/kroeser/schooljaar/Thesis/test-ldf/server-fork/bin/ldf-server"
+CLIENTDIR="/Users/kroeser/schooljaar/Thesis/test-ldf/client-fork/"
+
 # Set the triple formatting type.
-#TYPE="reification"
-TYPE="singletonproperties"
+TYPE="reification"
+#TYPE="singletonproperties"
 #TYPE="graphs"
 
 echo "Running format $TYPE"
 echo ""
 
-node generate $TYPE | sed "s/_:stmt/rdf:stmt/g" > radio.rdf
-./rdf2hdt -f n3 radio.rdf radio.hdt
-rm radio.hdt.index
-ldf-server config_hdt.json &
+node generate $TYPE > config.json
+$SERVER config.json &
 pid=$!
 sleep 1
 
@@ -19,5 +20,6 @@ echo "Executing continuous query..."
 echo ""
 echo ""
 
+export CLIENTDIR=$CLIENTDIR
 node query $TYPE
 kill -9 $pid
