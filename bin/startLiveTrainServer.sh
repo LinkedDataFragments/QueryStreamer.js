d4b077fabbff571d3d9eb30ea1fae14b197ded39
Ruben Taelman <rubensworks@gmail.com>
2014-11-22 15:53:11 +0100

    Implement iRail train delay demo.

--- bin/startLiveTrainServer.sh
+++ bin/startLiveTrainServer.sh
@@ -4,22 +4,22 @@ SERVER="/Users/kroeser/schooljaar/Thesis/test-ldf/server-fork/bin/ldf-server"
 CLIENTDIR="/Users/kroeser/schooljaar/Thesis/test-ldf/client-fork/"
 
 # Set the triple formatting type.
-#TYPE="reification"
+TYPE="reification"
 #TYPE="singletonproperties"
-TYPE="graphs"
+#TYPE="graphs"
 
 echo "Running format $TYPE"
 echo ""
 
-node generate $TYPE > config.json
-$SERVER config.json &
+export TYPE=$TYPE
+node live-ldf-server config_train.json &
 pid=$!
-sleep 1
+sleep 2
 
 echo "Executing continuous query..."
 echo ""
 echo ""
 
 export CLIENTDIR=$CLIENTDIR
-node query $TYPE
+node querytrain $TYPE
 kill -9 $pid
