c6dc134ab804cd65e9b85b8536a48f4fdb579e77
Ruben Taelman <rubensworks@gmail.com>
2014-12-30 17:51:06 +0100

    Implement query streaming silent mode with durationCallback.

--- bin/startLiveTrainServer.sh
+++ bin/startLiveTrainServer.sh
@@ -2,25 +2,36 @@
 
 SERVER="/Users/kroeser/schooljaar/Thesis/test-ldf/server-fork/bin/ldf-server"
 CLIENTDIR="/Users/kroeser/schooljaar/Thesis/test-ldf/client-fork/"
+DEBUG=true
+if $DEBUG; then export DEBUG=true; fi
 
 # Set the triple formatting type.
 #TYPE="reification"
 #TYPE="singletonproperties"
 TYPE="graphs"
 
-echo "Running format $TYPE"
-echo ""
-
 export TYPE=$TYPE
 export SERVER=$SERVER
-node live-ldf-server config_train.json &
+export CLIENTDIR=$CLIENTDIR
+
+if $DEBUG; then
+  echo "Running format $TYPE"
+  echo ""
+fi
+
+if $DEBUG; then
+  node live-ldf-server config_train.json &
+else
+  node live-ldf-server config_train.json > /dev/null &
+fi
 pid=$!
 sleep 5
 
-echo "Executing continuous query..."
-echo ""
-echo ""
+if $DEBUG; then
+  echo "Executing continuous query..."
+  echo ""
+  echo ""
+fi
 
-export CLIENTDIR=$CLIENTDIR
 node querytrain $TYPE
 kill -9 $pid
