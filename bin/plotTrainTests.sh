3e9eca994142497b0c50038effce2a1d114655cc
Ruben Taelman <rubensworks@gmail.com>
2015-01-10 14:57:02 +0100

    Double #plots to once include rewriting step.

--- bin/plotTrainTests.sh
+++ bin/plotTrainTests.sh
@@ -1,49 +1,66 @@
 #!/bin/zsh
 re='^[0-9]+$'
 
+mkdir -p plots
 dirs=$*
 
-for INTERVAL in true false; do
-  for CACHING in true false; do
-    unset matrix
-    typeset -A matrix
-    data=".train.data"
-    echo "Duration Reification Singletonproperties Graphs Implicitgraphs" > $data
-    maxi=0
-    for TYPE in reification singletonproperties graphs implicitgraphs; do
-      dircount=0
-      for dir in $dirs; do
-        let "dircount++"
-        input="$dir/annotation-"$TYPE"_interval-"$INTERVAL"_caching-"$CACHING".txt"
-        i=0
-        while read fline; do
-          value=${matrix[$i,$TYPE]}
-          if ! [[ $value =~ $re ]]; then value="0" ;fi
-          matrix[${i},${TYPE}]=$(echo "$fline + $value" | bc -l)
-          if [ "$i" -gt "$maxi" ]; then
-            maxi=$i
-          fi
-          let "i++"
-        done < <(tail -n +2 $input)
-      done
-    done
+for CUMULATIVE in true false; do
+  for REWRITING in true false; do
+    tailoffset="+2"
+    if $REWRITING; then
+      # This will include the first data line, the rewriting step
+      tailoffset="+1"
+    fi
+    for INTERVAL in true false; do
+      for CACHING in true false; do
+        unset matrix
+        typeset -A matrix
+        data=".train.data"
+        echo "Duration Reification Singletonproperties Graphs Implicitgraphs" > $data
+        maxi=0
+        for TYPE in reification singletonproperties graphs implicitgraphs; do
+          #echo $TYPE
+          dircount=0
+          for dir in $dirs; do
+            let "dircount++"
+            input="$dir/annotation-"$TYPE"_interval-"$INTERVAL"_caching-"$CACHING".txt"
+            i=0
+            while read fline; do
+              value=${matrix[$i,$TYPE]}
+              if ! [[ $value =~ $re ]]; then
+                if $CUMULATIVE && [ $i -gt 0 ]; then
+                  value=${matrix[$(echo "$i - 1" | bc -l),$TYPE]}
+                else
+                  value="0"
+                fi
+              fi
+              matrix[${i},${TYPE}]=$(echo "$fline + $value" | bc -l)
+              #echo "$fline + $value = ${matrix[$i,$TYPE]}"
+              if [ "$i" -gt "$maxi" ]; then
+                maxi=$i
+              fi
+              let "i++"
+            done < <(tail -n $tailoffset $input)
+          done
+        done
 
-    for i in $(seq 1 $maxi); do
-      line=$i
-      for TYPE in reification singletonproperties graphs implicitgraphs; do
-        value=${matrix[$i,$TYPE]}
-        if ! [[ $value =~ $re ]]; then
-          # makes sure the point isn't drawn
-          value="a"
-        else
-          value=$(echo "scale=2;$value / $dircount" | bc -l)
-        fi
-        line=$line" "$value
+        for i in $(seq 0 $maxi); do
+          line=$i
+          for TYPE in reification singletonproperties graphs implicitgraphs; do
+            value=${matrix[$i,$TYPE]}
+            if ! [[ $value =~ $re ]]; then
+              # makes sure the point isn't drawn
+              value="a"
+            else
+              value=$(echo "scale=2;$value / $dircount" | bc -l)
+            fi
+            line=$line" "$value
+          done
+          echo $line >> $data
+        done
+        file="plots/"$dir"_cumulative-"$CUMULATIVE"_interval-"$INTERVAL"_caching-"$CACHING"_rewriting-"$REWRITING".png"
+        gnuplot plotTrainTests.gplot > $file
       done
-      echo $line >> $data
     done
-    file="plots/"$dir"-interval-"$INTERVAL"_caching-"$CACHING".png"
-    mkdir -p plots
-    gnuplot plotTrainTests.gplot > $file
   done
 done
